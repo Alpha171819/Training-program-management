ced13e5963a341971cd334926a79346d9fcb3847
Srikanth <66662854+SrikanthKumarC@users.noreply.github.com>
2023-06-12 17:03:14 +0530

    bug fixes and typo in courses page

--- app/view_courses/page.js
+++ app/view_courses/page.js
@@ -2,8 +2,7 @@
 import React, { useEffect, useState } from "react";
 import styles from "../view_subjects/view_subjects.module.css";
 import axios from "axios";
-import Dashboard from '../dashboard/page';
-
+import Dashboard from "../dashboard/page";
 
 const Home = () => {
   const [data, setDate] = useState([]);
@@ -21,6 +20,18 @@ const Home = () => {
       setDate(updatedData);
     });
   }, [url]);
+
+  const deleteSubject = (id) => {
+    // alert to confirm delete
+    if (!confirm("Are you sure you want to delete this subject?")) return;
+    axios
+      .delete(`${url}/subject/${id}`)
+      .then((res) => {
+        console.log(res.data);
+        window.location.reload();
+      })
+      .catch((error) => console.log(error));
+  };
 
   function deleteCourse(id) {
     // alert to confirm delete
@@ -45,98 +56,111 @@ const Home = () => {
   return (
     <div>
       <Dashboard />
-    <div className={styles.container}>
-      <h2>Subjects</h2>
-      <ul className={styles["responsive-table"]}>
-        <li className={`${styles["table-header"]} ${styles.row} `}>
-          <div className={`${styles.col} ${styles["col-1"]}`}>Course ID</div>
-          <div className={`${styles.col} ${styles["col-2"]}`}>Course Name</div>
-          <div className={`${styles.col} ${styles["col-3"]}`}>
-            Course Officer
-          </div>
-          <div className={`${styles.col} ${styles["col-4"]}`}>
-            Number of Subjects
-          </div>
-          <div className={`${styles.col} ${styles["col-4"]}`}>
-            Course Strength
-          </div>
-          <div className={`${styles.col} ${styles["col-4"]}`}>End Date</div>
-          <div className={`${styles.col} ${styles["col-4"]}`}>
-            Duration (Weeks)
-          </div>
-          <div className={`${styles.col} ${styles["col-4"]}`}>Action</div>
-        </li>
-
-        {data?.map((item) => (
-          <li
-            key={item.course_id}
-            className={`${styles["table-row"]}  ${styles.row}`}
-          >
-            <div
-              className={`${styles.col} ${styles["col-1"]}`}
-              data-label="Job Id"
-            >
-              {" "}
-              {item.course_id}
+      <div className={styles.container}>
+        <h2>Courses</h2>
+        <ul className={styles["responsive-table"]}>
+          <li className={`${styles["table-header"]} ${styles.row} `}>
+            <div className={`${styles.col} ${styles["col-1"]}`}>Course ID</div>
+            <div className={`${styles.col} ${styles["col-2"]}`}>
+              Course Name
             </div>
-            <div
-              className={`${styles.col} ${styles["col-2"]}`}
-              data-label="Customer Name"
-            >
-              {item.course_name}
+            <div className={`${styles.col} ${styles["col-3"]}`}>
+              Course Officer
             </div>
-            <div
-              className={`${styles.col} ${styles["col-3"]}`}
-              data-label="Amount"
-            >
-              {" "}
-              {item.course_officer}
+            <div className={`${styles.col} ${styles["col-4"]}`}>
+              Number of Subjects
             </div>
-            <div
-              className={`${styles.col} ${styles["col-4"]}`}
-              data-label="Payment Status"
-            >
-              {item.no_subjects}
+            <div className={`${styles.col} ${styles["col-4"]}`}>
+              Course Strength
             </div>
-            <div
-              className={`${styles.col} ${styles["col-4"]}`}
-              data-label="Payment Status"
-            >
-              {item.course_strength}
+            <div className={`${styles.col} ${styles["col-4"]}`}>End Date</div>
+            <div className={`${styles.col} ${styles["col-4"]}`}>
+              Duration (Weeks)
             </div>
-            <div
-              className={`${styles.col} ${styles["col-4"]}`}
-              data-label="Payment Status"
-            >
-              {new Date(item.start_date).toLocaleDateString("hi-IN")}
-            </div>
-            <div
-              className={`${styles.col} ${styles["col-4"]}`}
-              data-label="Payment Status"
-            >
-              {new Date(item.end_date).toLocaleDateString("hi-IN")}
-            </div>
-            <div
-              className={`${styles.col} ${styles["col-4"]}`}
-              data-label="Payment Status"
-            >
-              {item.durationInWeeks}
-            </div>
-            <div
-              className={`${styles.col} ${styles["col-4"]} ${styles["flex"]} `}
-              data-label="Payment Status"
-            >
-              <button type="submit" className={styles.button} formAction="sub1">
-                ADD
-              </button>
-              <button type="submit" className={styles.button} formAction="sub2">
-                x
-              </button>
-            </div>
+            <div className={`${styles.col} ${styles["col-4"]}`}>Action</div>
           </li>
-        ))}
-      </ul>
-    </div>
+
+          {data?.map((item) => (
+            <li
+              key={item.course_id}
+              className={`${styles["table-row"]}  ${styles.row}`}
+            >
+              <div
+                className={`${styles.col} ${styles["col-1"]}`}
+                data-label="Job Id"
+              >
+                {" "}
+                {item.course_id}
+              </div>
+              <div
+                className={`${styles.col} ${styles["col-2"]}`}
+                data-label="Customer Name"
+              >
+                {item.course_name}
+              </div>
+              <div
+                className={`${styles.col} ${styles["col-3"]}`}
+                data-label="Amount"
+              >
+                {" "}
+                {item.course_officer}
+              </div>
+              <div
+                className={`${styles.col} ${styles["col-4"]}`}
+                data-label="Payment Status"
+              >
+                {item.no_subjects}
+              </div>
+              <div
+                className={`${styles.col} ${styles["col-4"]}`}
+                data-label="Payment Status"
+              >
+                {item.course_strength}
+              </div>
+              <div
+                className={`${styles.col} ${styles["col-4"]}`}
+                data-label="Payment Status"
+              >
+                {new Date(item.start_date).toLocaleDateString("hi-IN")}
+              </div>
+              <div
+                className={`${styles.col} ${styles["col-4"]}`}
+                data-label="Payment Status"
+              >
+                {new Date(item.end_date).toLocaleDateString("hi-IN")}
+              </div>
+              <div
+                className={`${styles.col} ${styles["col-4"]}`}
+                data-label="Payment Status"
+              >
+                {item.durationInWeeks}
+              </div>
+              <div
+                className={`${styles.col} ${styles["col-4"]} ${styles["flex"]} `}
+                data-label="Payment Status"
+              >
+                <button
+                  type="submit"
+                  className={styles.button}
+                  formAction="sub1"
+                >
+                  ADD
+                </button>
+                <button
+                  type="submit"
+                  onClick={() => {
+                    deleteCourse(item.course_id);
+                  }}
+                  className={styles.button}
+                  formAction="sub2"
+                >
+                  x
+                </button>
+              </div>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 };
