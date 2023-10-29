702de82e004e516edecde7aaf1e26e80dbae0280
SrikanthKumarC <wpsrikanth@gmail.com>
2023-10-29 17:52:04 +0530

    changes

--- app/piechart/page.js
+++ app/piechart/page.js
@@ -14,21 +14,25 @@ import { PieChart } from 'react-minimal-pie-chart';
 
 const Piechart = () => {
   const [runningSubjectsID, setRunningSubjectsID] = useState ([]);
+  console.log(" running subjects datassssssssssssssss ", runningSubjectsID);
 
     const [subjects, setSubjects] = useState([]);
+    const [completedSubjects, setCompletedSubjects] = useState ([]);
     const [selectedSubject, setSelectedSubject] = useState('');
 
     var total_subjects=32;
 
-
+    const [clickDisplay, setClickDisplay] = useState ([]);
 
     var uncompleted_subjects=runningSubjectsID.length;
     var completed_subjects=total_subjects-uncompleted_subjects -2;
 
-   
-  
-    const url = process.env.NEXT_PUBLIC_SERVER_URL;
 
+
+
+
+    const url = process.env.NEXT_PUBLIC_SERVER_URL;
+    const clickDisplayORsubjects = clickDisplay.length ? clickDisplay : subjects;
 
 //get the running subjects data
 useEffect(() => {
@@ -37,6 +41,13 @@ useEffect(() => {
       console.log(" running subjects data ", res.data);
       setRunningSubjectsID(res.data);
       console.log(" running subjects datassssssssssssssss ", runningSubjectsID);
+    });
+  }, [url]);
+  useEffect(() => {
+    axios.get(`${url}/completedSubjects`).then((res) => {
+      console.log(" running subjects data ", res.data);
+      setCompletedSubjects(res.data);
+      console.log(" running subjects datassssssssssssssss ", completedSubjects);
     });
   }, [url]);
 
@@ -51,18 +62,42 @@ useEffect(() => {
 
   return (
     <div>
-      <Dashboard /> 
-      <center>  <h1>Subjects Completion Status of DEE (NW)</h1></center>
-      <PieChart style={{height:"400px"}}
-  labelStyle = {{fontSize:"4px"}}
- 
-  label={({ dataEntry }) => dataEntry.title + ' ' + dataEntry.value}
-  data={[
-    { title: 'UnCompleted Subjects', value: uncompleted_subjects, color: 'red' },
-    { title: 'Completed Subjects', value: completed_subjects, color: 'green' },
-    { title: 'Running Subjects', value: 2, color: 'orange' }
-  ]}
-/>
+      <Dashboard />
+      <center>
+        {" "}
+        <h1>Subjects Completion Status of DEE (NW)</h1>
+      </center>
+      <PieChart
+        style={{ height: "400px" }}
+        onClick={(event, index) => {
+          switch (index) {
+            case 0:
+              setClickDisplay(runningSubjectsID);
+              break;
+            case 1:
+              setClickDisplay(completedSubjects);
+              break;
+            default:
+              break;
+          }
+          console.log("index", index);
+        }}
+        labelStyle={{ fontSize: "4px" }}
+        label={({ dataEntry }) => dataEntry.title + " " + dataEntry.value}
+        data={[
+          {
+            title: "UnCompleted Subjects",
+            value: uncompleted_subjects,
+            color: "red",
+          },
+          {
+            title: "Completed Subjects",
+            value: completed_subjects,
+            color: "green",
+          },
+          { title: "Running Subjects", value: 2, color: "orange" },
+        ]}
+      />
       <div className="container">
         <div className="row">
           <div className="col-md-12">
@@ -74,17 +109,17 @@ useEffect(() => {
                 </tr>
               </thead>
               <tbody>
-                {subjects.map((subject,idx) => (
+                {clickDisplayORsubjects.map((subject, idx) => (
                   <tr key={subject._id}>
                     <td>{subject.sub_name}</td>
                     <td>
-                      {
-                        runningSubjectsID.includes(subject.sub_id) ? (
-                          <span className="badge badge-danger">Not Completed</span>
-                        ) : (
-                          <span className="badge badge-success">Completed</span>
-                        )
-                      }
+                      {runningSubjectsID.includes(subject.sub_id) ? (
+                        <span className="badge badge-danger">
+                          Not Completed
+                        </span>
+                      ) : (
+                        <span className="badge badge-success">Completed</span>
+                      )}
                     </td>
                   </tr>
                 ))}
@@ -93,7 +128,6 @@ useEffect(() => {
           </div>
         </div>
       </div>
- 
     </div>
   );
 };
